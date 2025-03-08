30a70fee2d61eea57b03cc09560b19ed8f99e90a
tillenotuoma <totuoma27@gmail.com>
2025-03-08 11:05:51 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,25 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+session.query(Game).delete()
+session.commit()
+
+print("Seeding games...")
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    ) for _ in range(50)
+]
+
+# botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+# ffvii = Game(title="Final Fantasy VII", platform="PS1", genre="RPG", price=30)
+# mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+# ccs = Game(title="Candy Crush Saga", platform="Mobile", genre="Puzzle", price=0)
+
+session.bulk_save_objects(games)
+session.commit()
